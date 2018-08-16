7d835c46c5db11093894dec284c44fc83a0b96b3
Wasif Khan <waasif.khaan@gmail.com>
2018-08-16 02:23:16 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -48,7 +48,7 @@ Movie.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-Movie.all.find_by(id: 1)
+Movie.find_by(id: 1)
 end
 
 def can_find_by_multiple_attributes
@@ -56,13 +56,13 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.all.find_by(title: 'Title', release_date: 2000, director: 'Me')
+  Movie.find_by(title: 'Title', release_date: 2000, director: 'Me')
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  Movie.all.where("release_date> 2002").order(release_date: :desc)
+  Movie.where("release_date> 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
